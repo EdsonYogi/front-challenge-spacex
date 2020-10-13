7e4ed9640ddf61e307ca57cf37577ca9b89c60de
Edson Yogi <edson.yogi@hotmail.com>
2020-10-13 12:53:13 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-
+import Routes from './routes'
 import {ApolloProvider} from '@apollo/client'
 import client from './services/apollo'
-
-import Routes from './routes'
 
 function App() {
   return (
